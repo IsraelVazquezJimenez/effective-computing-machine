6dc8f700570939d1aeab615278fc8f686a449316
IsraelVazquezJimenez <isrvj2999@hotmail.com>
2025-09-18 00:57:48 +0000

    Cambios

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,17 +59,17 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
- """Test with n=10."""
- # Arrange
- n = 10
+   """Test with n=10."""
+   # Arrange
+   n = 10
 
- # Act
- result = get_nth_fibonacci(n)
+   # Act
+   result = get_nth_fibonacci(n)
 
- # Assert
- assert result == 55
+   # Assert
+   assert result == 55
 
- def test_area_of_circle_negative_radius():
+def test_area_of_circle_negative_radius():
   """Test with a negative radius to raise ValueError."""
   # Arrange
   radius = -1
@@ -78,7 +78,7 @@ def test_get_nth_fibonacci_ten():
   with pytest.raises(ValueError):
    area_of_circle(radius)
 
- def test_get_nth_fibonacci_negative():
+def test_get_nth_fibonacci_negative():
    """Test with a negative number to raise ValueError."""
    # Arrange
    n = -1
